29613a2fc91508b51d79976c4d5b1536255d6352
dahunsi-dami <gbemigadare@gmail.com>
2024-08-27 16:23:38 +0100

    Use Babel to set language, locale, timezone.

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Basic Flask app."""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    config for Flask app.
+    Sets available languages.
+    Sets Babel's default locale, timezone.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# We need to apply config to flask app now, so:
+app.config.from_object(Config)
+
+# Next, we instantiate the Babel object-
+# -for localization support.
+babel: Babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """
+    Index route for Flask app that renders 0-index.html
+    in templates dir.
+
+    Returns:
+        str: rendered HTML of 0-index.html.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
