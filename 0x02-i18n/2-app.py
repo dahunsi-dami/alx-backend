5e82d757124905b41536b97961cb8790695b23f0
dahunsi-dami <gbemigadare@gmail.com>
2024-08-27 16:46:36 +0100

    Do get_locale func to get best match in languages.

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Basic Flask app."""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    config for Flask app.
+    Sets available languages.
+    Sets Babel's default locale, timezone.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# We need to apply config to flask app now, so:
+app.config.from_object(Config)
+
+# Next, we instantiate the Babel object-
+# -for localization support.
+babel: Babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Picks best match w/ user's-
+    -supported language defined in Config class.
+
+    Returns:
+        str: the best match language code.
+    """
+    return request.accept_languages.best_match(
+        app.config['LANGUAGES']
+    )
+
+
+@app.route('/')
+def index():
+    """
+    Index route for Flask app that renders 0-index.html
+    in templates dir.
+
+    Returns:
+        str: rendered HTML of 0-index.html.
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
