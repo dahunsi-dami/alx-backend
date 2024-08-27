fd809f07a1694b1ab2474348f3f6da3829f1ddf8
dahunsi-dami <gbemigadare@gmail.com>
2024-08-27 16:29:55 +0100

    Make sure it renders 1-index.html instead.

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -34,7 +34,7 @@ def index():
     Returns:
         str: rendered HTML of 0-index.html.
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
